02f1a34330cd035fe9d6e9fcc9d364834842412d
Ecksi <jimmy.dechant@gmail.com>
2017-10-14 13:07:20 -0600

    Refactored to add initialize

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,17 +1,22 @@
 class KeyGenerator
 
+  def initialize
+    @key_to_array = generate_key.chars
+  end
+
   def generate_key
     5.times.map{rand(10)}.join
   end
 
   def a_rotation
-    a_key = generate_key.chars
-    2.times.map{a_key.shift}.join
+    # a_key = generate_key.chars
+    @key_to_array
+    2.times.map{@key_to_array.shift}.join
   end
 
   def b_rotation
-    b_key = generate_key.chars
+    @key_to_array
     b_key.shift
-    2.times.map{b_key.shift}.join
+    2.times.map{@key_to_array.shift}.join
   end
 end
