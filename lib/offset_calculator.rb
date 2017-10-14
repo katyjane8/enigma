dfde4ed1eb1accefe809d46f0e00248435fd13ce
Ecksi <jimmy.dechant@gmail.com>
2017-10-14 16:46:37 -0600

    tests for a-d_offset complete. create rotation test and ruby file

--- lib/offset_calculator.rb
+++ lib/offset_calculator.rb
@@ -10,9 +10,25 @@ class OffsetCalculator
   def date_squared(date)
     @date.to_i ** 2
   end
-
+  # should we rename our method so that we don't have a variable of the same name?
   def offset
-    offset = date_squared(date).to_s.chars
-    offset[-4, 4]
+    convert_offset = date_squared(date).to_s.chars
+    convert_offset[-4, 4]
+  end
+
+  def a_offset
+    offset[0]
+  end
+
+  def b_offset
+    offset[1]
+  end
+
+  def c_offset
+    offset[2]
+  end
+
+  def d_offset
+    offset[3]
   end
 end
--- test/offset_calculator_test.rb
+++ test/offset_calculator_test.rb
@@ -13,6 +13,7 @@ class OffsetCalculatorTest < Minitest::Test
   def test_date_squared
     oc = OffsetCalculator.new
     date = "101417"
+
     result = oc.date_squared(date)
 
     assert_equal 10285407889, result
@@ -21,19 +22,56 @@ class OffsetCalculatorTest < Minitest::Test
   def test_offset
     oc = OffsetCalculator.new
     date = "101417"
+
     result = oc.date_squared(date)
 
     assert_equal 4, oc.offset.length
     assert_equal "7889", oc.offset.join
   end
 
-
-#ask instructor if this needs to happen
+# ask instructor if this needs to happen
   # def test_another_date_works
   #   oc = OffsetCalculator.new
   #   date = "053117"
+  #
   #   result = oc.date_squared(date)
   #
   #   assert_equal 2821415689, result
   # end
+
+  def test_if_a_offset_grabs_index_zero
+    oc = OffsetCalculator.new
+    date = "101417"
+
+    result = oc.offset
+
+    assert_equal "7", result[0]
+  end
+
+  def test_if_b_offset_grabs_index_one
+    oc = OffsetCalculator.new
+    date = "101417"
+
+    result = oc.offset
+
+    assert_equal "8", result[1]
+  end
+
+  def test_if_c_offset_grabs_index_two
+    oc = OffsetCalculator.new
+    date = "101417"
+
+    result = oc.offset
+
+    assert_equal "8", result[2]
+  end
+
+  def test_if_d_offset_grabs_index_three
+    oc = OffsetCalculator.new
+    date = "101417"
+
+    result = oc.offset
+
+    assert_equal "9", result[3]
+  end
 end
