b44f64c05335b31411005572072211ef4f9977d2
Ecksi <jimmy.dechant@gmail.com>
2017-10-14 12:38:46 -0600

    first two tests passing

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,2 +1,17 @@
 class KeyGenerator
-end 
+
+  def generate_key
+    5.times.map{rand(9)}.join
+  end
+
+  def a_rotation
+    a_key = generate_key.chars
+    2.times.map{a_key.shift}.join
+  end
+
+  # def b_rotation
+  #   b_key = generate_key.chars
+  #   b_key.shift
+  #   2.times.map{b_key.shift}.join
+  # end
+end
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -1,3 +1,5 @@
+require 'minitest/autorun'
+require 'minitest/pride'
 require_relative '../lib/key_generator'
 require_relative 'test_helper'
 
@@ -5,7 +7,30 @@ class KeyGeneratorTest < Minitest::Test
 
   def test_it_exists
     kg = KeyGenerator.new
+
     assert_instance_of KeyGenerator, kg
   end
 
+  def test_we_can_generate_a_random_key
+    kg = KeyGenerator.new
+
+    result = kg.generate_key
+    assert_equal 5, result.length
+  end
+
+ def test_can_we_get_the_first_two_digits_of_our_key
+   kg = KeyGenerator.new
+
+   result = kg.generate_key.chars
+   assert_equal result[0], result.shift
+   assert_equal result[0], result.shift
+   assert_equal 3, result.length
+  end
+
+  def test_can_we_get_the_second_two_digits_of_our_key
+    skip
+    kg = KeyGenerator.new
+
+    assert_equal 5, kg.b_rotation
+  end
 end
