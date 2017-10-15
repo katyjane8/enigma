0e63e113ba3933ad648083bb3cc05c35b549ed63
Ecksi <jimmy.dechant@gmail.com>
2017-10-15 13:10:20 -0600

    meeting at the library

--- lib/rotation.rb
+++ lib/rotation.rb
@@ -2,19 +2,17 @@ require_relative 'key_generator'
 require_relative 'offset_calculator'
 
 class Rotation
-  attr_reader :key_generator,
-              :offset_calculator
+  attr_reader :key, :offset
 
   def initialize
-    @key_generator = Key_Generator.new
-    @offset_key= Offset_Calculator.new
-    @foo = foo
-    @bar = bar
+
   end
 
-  def first_rotation(@key_generator, @offset_key)
-    require "pry"; binding.pry
-    @key_generator.a_rotation + @offset_key.a_offset
+  def first_rotation(key, offset)
+    a_rotation + a_offset
   end
 
 end
+
+key = KeyGenerator.new
+offset = OffsetCalculator.new
--- test/rotation_test.rb
+++ test/rotation_test.rb
@@ -5,9 +5,26 @@ require_relative 'test_helper'
 
 class RotationTest < Minitest::Test
   def test_it_calls_my_classes
-    call_key = KeyGenerator.new
-    offset_key = OffsetCalculator.new
-    rotate = Rotation.new
-    rotate.first_rotation(call_key, offset_key)
+    key = KeyGenerator.new
+    offset = OffsetCalculator.new
+
+    assert_instance_of KeyGenerator, key
+    assert_instance_of OffsetCalculator, offset
+  end
+
+  def test_rotation_exists
+    rotation = Rotation.new
+
+    assert_instance_of Rotation, rotation
+  end
+
+  def test_first_rotation_works
+    kg = KeyGenerator.new
+    oc = OffsetCalculator.new
+    rotation = Rotation.new
+
+    result = kg.a_rotation + oc.a_offset
+
+    assert_equal "651", result
   end
 end
