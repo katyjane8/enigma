061d060d4791095aba75e98ea7bfa6f57cbbd518
Ecksi <jimmy.dechant@gmail.com>
2017-10-17 15:38:41 -0600

    encryption working

---
+++ lib/characters.rb
@@ -0,0 +1,18 @@
+module Characters
+  def cipher(rotation)
+    characters = ("a".."z").to_a + (0..9).to_a + (" ".."/").to_a
+    rotated_characters = characters.rotate(rotation)
+    Hash[characters.zip(rotated_characters)]
+  end
+
+  def encrypt_letter(letter, rotation)
+    cipher_for_rotation = cipher(rotation)
+    cipher_for_rotation[letter]
+  end
+
+  def encrypt(string, rotation)
+    letters = string.split("")
+    results = letters.collect { |letter| encrypted_letter = encrypt_letter(letter, rotation) }
+    results.join
+  end
+end
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,8 +1,9 @@
 require_relative 'offset_calculator'
 require_relative 'key_generator'
+require_relative 'characters'
 
 class Encrypt
-  include Rotater
+  include Characters
 
   attr_reader :offset_calc,
               :key_gen,
@@ -17,61 +18,43 @@ class Encrypt
   def a_splits(string)
     splitter = string.chars
     a_string = splitter.each_with_index.map { |splits, i| splits if i% 4 == 0 }
-    a_string.compact
+    a_string.compact.join
   end
 
-  def cipher(rotate = offset_calc.a_offset, string)
-    characters = ("a".."z").to_a
-    rotated_characters = characters.rotate(rotate)
-    Hash[characters.zip(rotated_characters)]
+  def a_rotated(string)
+    encrypt(a_splits(string), offset_calc.a_offset)
   end
 
   def b_splits(string)
     splitter = string.chars
     b_string = splitter.each_with_index.map { |splits, i| splits if i% 4 == 1 }
-    b_string.compact
+    b_string.compact.join
+  end
+
+  def b_rotated(string)
+    encrypt(b_splits(string), offset_calc.b_offset)
   end
 
   def c_splits(string)
     splitter = string.chars
     c_string = splitter.each_with_index.map { |splits, i| splits if i% 4 == 2 }
-    c_string.compact
+    c_string.compact.join
+  end
+
+  def c_rotated(string)
+    encrypt(c_splits(string), offset_calc.c_offset)
   end
 
   def d_splits(string)
     splitter = string.chars
     d_string = splitter.each_with_index.map { |splits, i| splits if i% 4 == 3 }
-    d_string.compact
+    d_string.compact.join
   end
-end
 
-  # def alla_shazam(rotation)
-  # call cipher module - input oc.a_offset as argument
-  # use that rotated char map on a_string and swap characters
-  # end
+  def d_rotated(string)
+    encrypt(d_splits(string), offset_calc.d_offset)
+  end
 
-  # def cipher(rotation = oc.offset)
-  #   characters = ("a".."z").to_a
-  #   rotated_characters = characters.rotate(rotation)
-  #   Hash[characters.zip(rotated_characters)]
-  # end
-
-  # def encrypt_letter(letter, rotation = oc.offset)
-  #   cipher_for_rotation = cipher(rotation)
-  #   cipher_for_rotation[letter]
-  # end
-  #
-  # def encrypt(string, rotation = oc.offset)
-  #   letters = string.split("")
-  #   results = letters.collect { |letter| encrypted_letter = encrypt_letter(letter, rotation) }
-  #   results.join
-  # end
-  #
-  # def decrypt(string, rotation = -(oc.offset), j)
-  #    letters = string.split("")
-  #    results = letters.collect { |letter| encrypted_letter = encrypt_letter(letter, rotation) }
-  #    results.join
-  #  end
 
   def output
     puts "Created -- with the key #{key_gen.key_output} and date #{offset_calc.date_format}."
@@ -79,15 +62,19 @@ end
 end
 enc = Encrypt.new
 oc = OffsetCalculator.new
-# puts enc.encrypt_letter("e")
-# puts enc.encrypt("help help help")
-# puts enc.encrypt("ohh Shit we got encrpytion")
+
 puts enc.output
 print enc.a_splits("try this string"); puts ""
-print enc.a_funky_chicken("try this string"); puts ""
-# chegg_this_out = Hash[enc.a_splits("trythisstring").zip(enc.a_funky_chicken("trythisstring"))]
-# p chegg_this_out
-# p chegg_this_out.invert
-print enc.b_splits("try this string"); puts ""
-print enc.c_splits("try this string"); puts ""
-print enc.d_splits("try this string"); puts ""
+print enc.a_rotated("try this string"); puts ""; puts ""
+
+# print enc.b_splits("try this string"); puts ""
+# print enc.b_rotated("try this string"); puts ""; puts ""
+#
+# print enc.c_splits("try this string"); puts ""
+# print enc.c_rotated("try this string"); puts ""; puts ""
+#
+# print enc.d_splits("try this string"); puts ""
+# print enc.d_rotated("try this string"); puts ""; puts ""
+
+
+# puts "#{enc.a_rotated("try this string") + enc.b_rotated("try this string") + enc.c_rotated("try this string") + enc.d_rotated("try this string")}"
